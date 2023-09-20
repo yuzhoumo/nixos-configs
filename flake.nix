c1595af2845543dd287203f42d89a55a2c77a05f
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-09-20 16:38:30 -0700

    feat(ghost): add initial vm host configs

--- flake.nix
+++ flake.nix
@@ -24,10 +24,31 @@
 
     # available through 'nixos-rebuild --flake .#hostname'
     nixosConfigurations = {
+
+	  # Virtual Machine
+      ghost = nixpkgs.lib.nixosSystem {
+        modules = [ ./hosts/ghost ];
+        specialArgs = { inherit inputs outputs; };
+      };
+
+      # Desktop
       navi = nixpkgs.lib.nixosSystem {
         modules = [ ./hosts/navi ];
         specialArgs = { inherit inputs outputs; };
       };
+
+	  # TODO: Macbook
+      # spinner = nixpkgs.lib.nixosSystem {
+      #   modules = [ ./hosts/spinner ];
+      #   specialArgs = { inherit inputs outputs; };
+      # };
+
+	  # TODO: Home Server
+      # offworld = nixpkgs.lib.nixosSystem {
+      #   modules = [ ./hosts/offworld ];
+      #   specialArgs = { inherit inputs outputs; };
+      # };
+
     };
 
     # homeConfigurations = {
---
+++ hosts/ghost/default.nix
@@ -0,0 +1,82 @@
+# Host-specific system configurations for esper (desktop)
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+	../../common/all
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader = {
+      grub = {
+        enable = true;
+        grub.device = "/dev/sda";
+      }
+    };
+  };
+
+  networking = {
+    hostName = "ghost";
+    networkmanager.enable = true;
+  };
+
+  # Enable X11 windowing system
+  services.xserver = {
+    enable = true;
+
+  #   displayManager = {
+  #     defaultSession = "none+i3";
+  #     sessionCommands = ''
+		# xrandr --output DP-0 --mode 1920x1080 --rate 144
+  #     '';  # Connect to primary 144hz monitor
+  #   };
+
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      extraPackages = with pkgs; [
+        i3lock  # Default i3 screen locker
+        rofi    # Application launcher
+      ];
+    };
+
+    # Configure X11 keymap
+    layout = "us";
+  };
+
+  # Enable dconf
+  programs.dconf.enable = true;
+
+  # Define user account
+  users.users.ppanda = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];  # Enable ‘sudo’ for the user.
+	shell = pkgs.zsh;
+    packages = with pkgs; [
+      bitwarden
+      deluge
+      discord
+	  feh
+      firefox
+      gimp
+      gnome.nautilus
+      neovim
+      obsidian
+      pavucontrol
+      pcmanfm
+      picom
+      (polybar.override { i3Support = true; pulseSupport = true; })
+      shotwell
+      signal-desktop
+      slack
+      spotify
+      tor-browser-bundle-bin
+      vlc
+    ];
+  };
+
+  system.stateVersion = "22.11";
+}
